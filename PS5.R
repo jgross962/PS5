c9d23baf06e84388cc1de7767838bb4235ff46b1
JGross <Jonathan.Gross@wustl.edu>
2018-03-04 17:46:36 -0600

    Trapezoid Working
    Trapezoid Working. Put in as S3 Function -- will need to convert to S4 Later.

--- PS5.R
+++ PS5.R
@@ -2,32 +2,66 @@
 # PS5
 
 setGeneric("integrateIt",
-           function(object = "door"){
+           function(x = "numeric",
+                    y = "numeric",
+                    ){
              standardGeneric("integrateIt")
            })
 
-x = 1:10
-y = 30:40
 
-firstVal = 1
-lastVal = 10
+setMethod("integrateIt","x","y",c(a,b)
+          function(object){
+            #Create the winning door with random value
+            WinningDoor = new("Door",DoorNumber = floor(runif(1,1,4)))
+            #Print Appropriate Output
+            if(WinningDoor@DoorNumber == object@DoorNumber){
+              return("Congratulations You've Won A Car")
+            }else{
+              return("I'm Sorry You Didn't Win")
+            }
+          })
 
-integrateIt = Function(x,y,a = min(x), b = max(x), rule){
- 
+integrateIt = function(x,y,a,b,rule){
+  
+  
   ## cREATE rule to throw error if x is not sorted
   ## Create rule to make sure x and y are same length
   
-  
-  if (rule = "Trap"){
-    area = 0
-    for (i in x[firstVal+1]:x[lastVal]){
-      width = x[i]-x[i-1]
-      height1 = y[i-1]
-      height2 = y[i]
-      area = area+ 1/2*width*(height1+height2)
-      
-    } 
+  # Trapezoid Rule
+  if (rule == "Trap"){
+    b = x[length(x)]
+    a = x[1]
+    width = (b-a)/(length(x)-1)
+    T = width*1/2*(sum(2*y)-y[1]-y[length(y)])
+    return(T)
+    
+    
   }
-  
-  
 }
+
+
+firstVal = 1
+lastVal = 10
+x = 1:10
+y = 30:40
+integrateIt(x,y,firstVal,lastVal,"Trap")
+
+x = 1:10
+y = rep(1,10)
+integrateIt(x,y,firstVal,lastVal,"Trap")
+
+x = 1:4
+y = 1:4
+integrateIt(x,y,firstVal,lastVal,"Trap")
+
+## Actual Trapezoid Rule
+  area=0
+  for (i in x[2]:x[length(x)]){
+    width = x[i]-x[i-1]
+    height1 = y[i-1]
+    height2 = y[i]
+    area <<- area+ 1/2*width*(height1+height2)
+  } 
+  area
+  
+
